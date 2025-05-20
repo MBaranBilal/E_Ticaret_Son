d735698d1d17386b8d11c74e7ef4b86d2a678a62
M.Baran Bilal <mahfuzbaranbilal@gmail.com>
2025-05-20 20:30:17 +0300

    Add project files.

---
+++ E_Ticaret/Controllers/ShopController.cs
@@ -0,0 +1,25 @@
+﻿using E_Ticaret.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Ticaret.Controllers
+{
+	public class ShopController : Controller
+	{
+		ETicaretDbContext context = new ETicaretDbContext();
+		public IActionResult Shop(double? minPrice, double? maxPrice)
+		{
+			var products = context.Products.AsQueryable();
+
+			if (minPrice != null)
+				products = products.Where(p => p.Price >= minPrice);
+
+			if (maxPrice != null)
+				products = products.Where(p => p.Price <= maxPrice);
+
+			var values = products.ToList();
+			return View(values);
+		}
+
+	}
+}
